447fe54cf17cf36e28c9eca673ea7532c6ff997a
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-10 15:37:28 +0300

    Added and implemented a card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,16 @@
 <script>
-
-
-    import {Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte"
+    import {
+        Button,
+        Card,
+        Navbar,
+        NavBrand,
+        NavHamburger,
+        NavLi,
+        NavUl,
+    } from "flowbite-svelte";
 </script>
 
-
-
 <Navbar fluid={true}>
-
     <NavBrand>
         <span class="text-xl font-bold">Abdulrahman</span>
     </NavBrand>
@@ -15,12 +18,19 @@
     <NavHamburger></NavHamburger>
 
     <NavUl>
-
-        <NavLi> Home</NavLi>
-        <NavLi> About me</NavLi>
-        <NavLi> Projects</NavLi>
-        <NavLi> Contact me</NavLi>
+        <NavLi>Home</NavLi>
+        <NavLi>About me</NavLi>
+        <NavLi>Projects</NavLi>
+        <NavLi>Contact me</NavLi>
     </NavUl>
 </Navbar>
 
 
+
+<div
+    class="w-full grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center-safe"
+>
+    {#each Array(100) as arr}
+        <Card class="w-full">Hola senior</Card>
+    {/each}
+</div>
---
+++ src/routes/Card.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { Button, Card } from "flowbite-svelte";
+    
+</script>
+<div class="flex flex-col">
+    <Card img="/images/image-1.webp">
+        <div class="m-6">
+            <h5
+                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+                Noteworthy technology acquisitions 2021
+            </h5>
+            <p
+                class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400"
+            >
+                Here are the biggest enterprise technology acquisitions of 2021
+                so far, in reverse chronological order.
+            </p>
+            <Button class="w-40">
+                Read more
+            </Button>
+        </div>
+    </Card>
+</div>
