aad24b1502ba74e2c70410fbec350841928c4af2
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-10 15:21:01 +0300

    Added navigation bar to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+
+    import {Navbar, NavBrand, NavHamburger, NavLi, NavUl} from "flowbite-svelte"
+</script>
+
+
+
+<Navbar fluid={true}>
+
+    <NavBrand>
+        <span class="text-xl font-bold">Abdulrahman</span>
+    </NavBrand>
+
+    <NavHamburger></NavHamburger>
+
+    <NavUl>
+
+        <NavLi> Home</NavLi>
+        <NavLi> About me</NavLi>
+        <NavLi> Projects</NavLi>
+        <NavLi> Contact me</NavLi>
+    </NavUl>
+</Navbar>
+
+
