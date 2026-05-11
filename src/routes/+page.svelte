e6dd0838af63754287ee921e99405b6ede314dd1
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2026-05-11 09:21:12 +0300

    Rearanged the contact me and projects in progress section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import {
-    A,
+        A,
         Navbar,
         NavBrand,
         NavHamburger,
@@ -17,12 +17,9 @@
 
     onMount(async () => {
         let data = await fetch("/portfolio/projects.json");
-        projects = (await data.json());
+        projects = await data.json();
         console.log(projects[0]);
-        
     });
-
-
 </script>
 
 <Navbar fluid={true}>
@@ -44,7 +41,7 @@
     <div class="w-full text-lg font-semibold">
         About me
 
-        <P class="font-light text-md" >
+        <P class="font-light text-md">
             I’m a computer engineer focused on QA automation, backend
             development, and development tooling. My primary focus is building
             reliable testing workflows using Playwright, Docker, GitHub Actions,
@@ -62,53 +59,67 @@
         class="w-full grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center-safe"
     >
         {#each projects as project}
-            <Card title={project.projectName} description={project.projectDesc} briefDescription={project.projectBriefDesc} link={project.projectLink} imageLink={project.image}></Card>
+            <Card
+                title={project.projectName}
+                description={project.projectDesc}
+                briefDescription={project.projectBriefDesc}
+                link={project.projectLink}
+                imageLink={project.image}
+            ></Card>
         {/each}
     </div>
 
-
-
-        <div class="w-full text-lg font-semibold" id="contact-me">
-        Contact me
-
-        <P class="font-light text-md" >
-        My email : abdulrahman.7.2k@gmail.com
-        github : <A href="https://github.com/abody2k"> over here!</A>
-        Phone number : +9647744415507
-        WhatsApp : <A href="https://wa.link/343zey"> Click!</A>
-
-        </P>
-    </div>
-
-
-
-
-
-
-
-        <div class="w-full text-lg font-semibold">
+    <div class="w-full text-lg font-semibold">
         Ongoing projects
 
-<Timeline order="vertical">
-  <TimelineItem title="Pack Mann" date="Started on May 7 2026">
-    {#snippet orientationSlot()}
-      <span class="bg-primary-200 dark:bg-primary-900 absolute -left-4 flex h-6 w-6 items-center justify-center rounded-full ring-8 ring-white dark:ring-gray-900">
-      </span>
-    {/snippet}
-    <p class="mb-4 pl-4 text-base font-normal text-gray-500 dark:text-gray-400">
-      Started with an empty project
-    </p>
-  </TimelineItem>
-  <TimelineItem title="Pack Mann early build" date="To be released on May 20 2026">
-    {#snippet orientationSlot()}
-      <span class="bg-primary-200 dark:bg-primary-900 absolute -left-4 flex h-6 w-6 items-center justify-center rounded-full ring-8 ring-white dark:ring-gray-900">
-      </span>
-    {/snippet}
-    <p class="pl-4 text-base font-normal text-gray-500 dark:text-gray-400">
-        Includes controller, playing and multiplayer logic. on top of using docker and github actions to auto deploy
-    </p>
-  </TimelineItem>
+        <Timeline order="vertical">
+            <TimelineItem title="Pack Mann" date="Started on May 7 2026">
+                {#snippet orientationSlot()}
+                    <span
+                        class="bg-primary-200 dark:bg-primary-900 absolute -left-4 flex h-6 w-6 items-center justify-center rounded-full ring-8 ring-white dark:ring-gray-900"
+                    >
+                    </span>
+                {/snippet}
+                <p
+                    class="mb-4 pl-4 text-base font-normal text-gray-500 dark:text-gray-400"
+                >
+                    Started with an empty project
+                </p>
+            </TimelineItem>
+            <TimelineItem
+                title="Pack Mann early build"
+                date="To be released on May 20 2026"
+            >
+                {#snippet orientationSlot()}
+                    <span
+                        class="bg-primary-200 dark:bg-primary-900 absolute -left-4 flex h-6 w-6 items-center justify-center rounded-full ring-8 ring-white dark:ring-gray-900"
+                    >
+                    </span>
+                {/snippet}
+                <p
+                    class="pl-4 text-base font-normal text-gray-500 dark:text-gray-400"
+                >
+                    Includes controller, playing and multiplayer logic. on top
+                    of using docker and github actions to auto deploy
+                </p>
+            </TimelineItem>
+        </Timeline>
+    </div>
 
-</Timeline>
+    <div class="w-full text-lg font-semibold" id="contact-me">
+        Contact me
+
+        <P class="font-light text-md">
+            My email : abdulrahman.7.2k@gmail.com github : <A
+                href="https://github.com/abody2k"
+            >
+                over here!</A
+            >
+            Phone number : +9647744415507 WhatsApp : <A
+                href="https://wa.link/343zey"
+            >
+                Click!</A
+            >
+        </P>
     </div>
 </div>
